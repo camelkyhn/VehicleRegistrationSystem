26101b10be5980cfca1a56cf169729f3ffb7d355
Kemal <m.kemal.kayahan@hotmail.com>
2018-06-28 19:03:12 +0300

    Dataaccess interface

---
+++ VehicleRegistrationSystem.Common/DataAccess/IDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace VehicleRegistrationSystem.Common.DataAccess
+{
+    public interface IDataAccess<T>
+    {
+        int Insert(T entity);
+        int Delete(T entity);
+        int Update(T entity);
+        bool Any(Expression<Func<T, bool>> expression);
+        T Find(Expression<Func<T, bool>> expression);
+        List<T> GetList();
+        List<T> GetList(Expression<Func<T, bool>> expression);
+        void Dispose();
+        int Save();
+    }
+}
