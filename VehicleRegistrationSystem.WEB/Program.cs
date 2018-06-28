a16f745741d25cb680af0f376894f06c87b42851
Kemal <m.kemal.kayahan@hotmail.com>
2018-06-28 19:35:11 +0300

    Added initialization to program.cs

--- VehicleRegistrationSystem.WEB/Program.cs
+++ VehicleRegistrationSystem.WEB/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using VehicleRegistrationSystem.DAL.Context;
 
 namespace VehicleRegistrationSystem.WEB
 {
@@ -14,7 +11,24 @@ namespace VehicleRegistrationSystem.WEB
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DatabaseContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception exception)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exception, "An error occurred while seeding the database.");
+                }
+            }
+            
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
