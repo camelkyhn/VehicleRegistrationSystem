948d4f166f69d6a593bfbbe33900604b9fed09cb
Kemal <m.kemal.kayahan@hotmail.com>
2018-06-28 18:54:42 +0300

    Entities created

---
+++ VehicleRegistrationSystem.Common/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VehicleRegistrationSystem.Common.Entities
+{
+    public class Entity
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ VehicleRegistrationSystem.Common/Entities/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VehicleRegistrationSystem.Common.Entities
+{
+    public class Vehicle
+    {
+        public string Plate { get; set; }
+        
+        public string NickName { get; set; }
+        
+        public string Brand { get; set; }
+        
+        public string Model { get; set; }
+        
+        public int? ModelYear { get; set; }
+        
+        public string Type { get; set; }
+        
+        public string Color { get; set; }
+        
+        public bool? IsActive { get; set; }
+    }
+}
